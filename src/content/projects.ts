919b0e5ec006971b2406e535946da0039db1d8ad
dimfu <dimmfu@gmail.com>
2025-01-28 12:34:56 +0800

    add spade as selected project

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -5,7 +5,8 @@ interface Project {
 }
 
 export const Projects: Project[] = [
-    {name: "KBT-leaderboard", description: "Unofficial Touge Union's KBT leaderboard notification", url: "https://github.com/dimfu/KBT-leaderboard"},
-    {name: "recipe-scraper", description: "Tiny package to scrap food recipe from the web", url:"https://github.com/dimfu/recipe-scraper"},
-    {name: "sdl-go", description: "Subtitle donwloader for your torrent movies", url: "https://github.com/dimfu/sdl-go"},
+    {name: "spade", description: "Discord bot for holding general tournament bracket.", url: "https://github.com/dimfu/spade"},
+    {name: "KBT-leaderboard", description: "Unofficial Touge Union's KBT leaderboard notification.", url: "https://github.com/dimfu/KBT-leaderboard"},
+    {name: "recipe-scraper", description: "Tiny package to scrap food recipe from the web.", url:"https://github.com/dimfu/recipe-scraper"},
+    {name: "sdl-go", description: "Subtitle donwloader for your torrent movies.", url: "https://github.com/dimfu/sdl-go"},
 ]
