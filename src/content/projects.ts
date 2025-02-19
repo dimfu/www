b6cb5256ef5de6cff0de460f9181140ebb0714f8
dimfu <dimmfu@gmail.com>
2025-02-19 21:27:35 +0800

    edit selected project info

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -6,7 +6,7 @@ interface Project {
 
 export const Projects: Project[] = [
     {name: "spade", description: "Discord bot for holding general tournament bracket.", url: "https://github.com/dimfu/spade"},
-    {name: "KBT-leaderboard", description: "Unofficial Touge Union's KBT leaderboard notification.", url: "https://github.com/dimfu/KBT-leaderboard"},
+    {name: "kaido", description: "Unofficial Touge Union Leaderboard Notification.", url: "https://github.com/dimfu/kaido"},
     {name: "recipe-scraper", description: "Tiny package to scrap food recipe from the web.", url:"https://github.com/dimfu/recipe-scraper"},
     {name: "sdl-go", description: "Subtitle donwloader for your torrent movies.", url: "https://github.com/dimfu/sdl-go"},
 ]
