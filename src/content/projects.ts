a313efd3a6f2d3c8b90d6740a610ccc4dc9e1d9f
dimfu <dimmfu@gmail.com>
2025-03-15 17:38:37 +0800

    replace recipe-scraper with apron

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -5,8 +5,8 @@ interface Project {
 }
 
 export const Projects: Project[] = [
-    {name: "spade", description: "Discord bot for holding general tournament bracket.", url: "https://github.com/dimfu/spade"},
-    {name: "kaido", description: "Unofficial Touge Union Leaderboard Notification.", url: "https://github.com/dimfu/kaido"},
-    {name: "recipe-scraper", description: "Tiny package to scrap food recipe from the web.", url:"https://github.com/dimfu/recipe-scraper"},
-    {name: "sdl-go", description: "Subtitle donwloader for your torrent movies.", url: "https://github.com/dimfu/sdl-go"},
+    { name: "spade", description: "Discord bot for holding general tournament bracket.", url: "https://github.com/dimfu/spade" },
+    { name: "kaido", description: "Unofficial Touge Union Leaderboard Notification.", url: "https://github.com/dimfu/kaido" },
+    { name: "apron", description: "Write recipe that computer can understand", url: "https://github.com/dimfu/apron" },
+    { name: "sdl-go", description: "Subtitle donwloader for your torrent movies.", url: "https://github.com/dimfu/sdl-go" },
 ]
