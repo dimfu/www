243ee26a1215637d30682b98389a24697a90b40f
dimfu <dimmfu@gmail.com>
2025-05-12 16:19:40 +0800

    add mrwordcount to project list

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -9,4 +9,5 @@ export const Projects: Project[] = [
     { name: "kaido", description: "Unofficial Touge Union Leaderboard Notification.", url: "https://github.com/dimfu/kaido" },
     { name: "apron", description: "Write recipe that computer can understand", url: "https://github.com/dimfu/apron" },
     { name: "sdl-go", description: "Subtitle donwloader for your torrent movies.", url: "https://github.com/dimfu/sdl-go" },
+    { name: "mrwordcount", description: "Word count map reduce implementation in Golang", url: "https://github.com/dimfu/mrwordcount" },
 ]
