521da59838551477564e0473ad7104ed6fd2369b
dimfu <dimmfu@gmail.com>
2025-07-01 20:49:31 +0800

    configure project with cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  adapter: cloudflare()
 });
