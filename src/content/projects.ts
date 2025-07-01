0678317fa9b37c595a932434ad012a5ac7b3cd8c
dimfu <dimmfu@gmail.com>
2025-07-01 20:15:39 +0800

    update featured projects list

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -1,13 +1,7 @@
-interface Project {
-    name: string
-    description: string
-    url: string
-}
-
-export const Projects: Project[] = [
+export const Projects = [
     { name: "spade", description: "Discord bot for holding general tournament bracket.", url: "https://github.com/dimfu/spade" },
     { name: "kaido", description: "Unofficial Touge Union Leaderboard Notification.", url: "https://github.com/dimfu/kaido" },
-    { name: "apron", description: "Write recipe that computer can understand", url: "https://github.com/dimfu/apron" },
-    { name: "sdl-go", description: "Subtitle donwloader for your torrent movies.", url: "https://github.com/dimfu/sdl-go" },
-    { name: "mrwordcount", description: "Word count map reduce implementation in Golang", url: "https://github.com/dimfu/mrwordcount" },
-]
+    { name: "apron", description: "Write recipe that computer can understand.", url: "https://github.com/dimfu/apron" },
+    { name: "mrwordcount", description: "Word count map reduce implementation in Golang.", url: "https://github.com/got-many-wheels/mrwordcount" },
+    { name: "spoderman", description: "A Dead simple, configurable and fast web crawler to grab specific data using regular expression.", url: "https://github.com/got-many-wheels/spoderman" },
+] as const
